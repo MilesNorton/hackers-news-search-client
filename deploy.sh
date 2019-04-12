71e20343a797be7904590b3196940365e3ef375f
Miles Norton <carlfoster@localhost.localdomain>
2019-04-12 10:57:53 +1000

    test

--- deploy.sh
+++ deploy.sh
@@ -39,14 +39,14 @@ docker push ${TARGET_IMAGE_VERSIONED}
 
 ###### ECS deployment #######
 # Deploy only if it's not a pull request
-if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-# Deploy only if we're testing the master branch
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-echo "Deploying $TRAVIS_BRANCH on $TASK_DEFINITION"
-./ecs-deploy -c $TASK_DEFINITION -n $SERVICE -i $REGISTRY_URL:$TRAVIS_BRANCH
-else
-echo "Skipping deploy because it's not an allowed branch"
-fi
-else
-echo "Skipping deploy because it's a PR"
-fi
+# if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+# # Deploy only if we're testing the master branch
+# if [ "$TRAVIS_BRANCH" == "master" ]; then
+# echo "Deploying $TRAVIS_BRANCH on $TASK_DEFINITION"
+# bash ./ecs-deploy -c $TASK_DEFINITION -n $SERVICE -i $REGISTRY_URL:$TRAVIS_BRANCH
+# else
+# echo "Skipping deploy because it's not an allowed branch"
+# fi
+# else
+# echo "Skipping deploy because it's a PR"
+# fi
