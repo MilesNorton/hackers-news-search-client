7bc41dbf5087645a6701d57b8e404659232a1730
Miles Norton <carlfoster@localhost.localdomain>
2019-04-12 12:41:11 +1000

    test

--- src/app/components/SearchClient.tsx
+++ src/app/components/SearchClient.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { ISearchResults, ISearchResult } from "../lib/interface";
+import * as React from 'react'
+import { ISearchResults, ISearchResult } from '../lib/interface'
 import {
   Segment,
   Grid,
@@ -7,28 +7,28 @@ import {
   Input,
   Divider,
   Container,
-  PaginationProps
-} from "semantic-ui-react";
-import { fetchData } from "../lib/apiCalls";
-import { fetchResults, dispatchInterface } from "../actions/queryActions";
-import { connect } from "react-redux";
-import SearchResult from "./SearchResult";
-import FailedResult from "./FailedResult";
-import { useState, useEffect } from "react";
-import Pagination from "./Pagination";
-import { IState } from "../reducers/queryReducer";
-import { Dispatch } from "redux";
-const MAXPERPAGE = 5; // can add a variable for changing the per page view value
-const QUERY_URL = "http://hn.algolia.com/api/v1/search?";
+  PaginationProps,
+} from 'semantic-ui-react'
+import { fetchData } from '../lib/apiCalls'
+import { fetchResults, dispatchInterface } from '../actions/queryActions'
+import { connect } from 'react-redux'
+import SearchResult from './SearchResult'
+import FailedResult from './FailedResult'
+import { useState, useEffect } from 'react'
+import Pagination from './Pagination'
+import { IState } from '../reducers/queryReducer'
+import { Dispatch } from 'redux'
+const MAXPERPAGE = 5 // can add a variable for changing the per page view value
+const QUERY_URL = 'http://hn.algolia.com/api/v1/search?'
 interface IProps {
-  results: ISearchResults | undefined;
-  activePage: number;
-  queriedTotalPages: number;
-  fetchedResults: (url: string) => Promise<dispatchInterface>;
+  results: ISearchResults | undefined
+  activePage: number
+  queriedTotalPages: number
+  fetchedResults: (url: string) => Promise<dispatchInterface>
 }
 
 interface IQueryState {
-  query: IState;
+  query: IState
 }
 
 function SearchClient(props: IProps): JSX.Element {
@@ -37,129 +37,130 @@ function SearchClient(props: IProps): JSX.Element {
     totalPages,
     currentPage,
     handlePageChange,
-    handleSearchChange
-  } = useStateChange(props);
+    handleSearchChange,
+  } = useStateChange(props)
 
   return (
-    <div className="search-client-outer-container">
-      <Grid textAlign="center" className="search-client-outer-grid">
-        <Grid.Column className="search-client-inner-grid">
+    <div className='search-client-outer-container'>
+      hello
+      <Grid textAlign='center' className='search-client-outer-grid'>
+        <Grid.Column className='search-client-inner-grid'>
           <Divider />
           <Input
             fluid
             onChange={handleSearchChange}
-            icon={<Icon name="search" inverted circular link />}
-            placeholder="Search..."
+            icon={<Icon name='search' inverted circular link />}
+            placeholder='Search...'
           />
           <Divider />
-          <Segment.Group id="results-container">
+          <Segment.Group id='results-container'>
             {queriedResults(searchTerm, SearchResult, {
-              results: props.results
+              results: props.results,
             })}
           </Segment.Group>
           <Divider />
-          <Container textAlign="center">
+          <Container textAlign='center'>
             {Pagination(currentPage, totalPages, handlePageChange)}
           </Container>
           <Divider />
         </Grid.Column>
       </Grid>
     </div>
-  );
+  )
 }
 function useStateChange(props: IProps) {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [totalPages, setTotalPages] = useState(1);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('')
+  const [totalPages, setTotalPages] = useState(1)
+  const [currentPage, setCurrentPage] = useState(1)
 
   const handlePageChange = (
     e: React.MouseEvent<HTMLAnchorElement>,
     data: PaginationProps
   ): void => {
-    setCurrentPage(Number(data.activePage)); //reset current page on each search
+    setCurrentPage(Number(data.activePage)) //reset current page on each search
     fetchData(
       QUERY_URL,
       MAXPERPAGE,
       searchTerm,
       currentPage,
       props.fetchedResults
-    );
+    )
     //   (searchTerm, currentPage, props.fetchedResults); // remember to decrement the active page by 1 for the query
-  };
+  }
 
   const handleSearchChange = (e: any) => {
-    setSearchTerm(e.target.value); //update search term text as the user is typing
-    setCurrentPage(1); //reset current page on each search
+    setSearchTerm(e.target.value) //update search term text as the user is typing
+    setCurrentPage(1) //reset current page on each search
     fetchData(
       QUERY_URL,
       MAXPERPAGE,
       searchTerm,
       currentPage,
       props.fetchedResults
-    );
-  };
+    )
+  }
 
   useEffect(() => {
-    setTotalPages(props.queriedTotalPages);
-    return () => setTotalPages(1);
-  }, [props]);
+    setTotalPages(props.queriedTotalPages)
+    return () => setTotalPages(1)
+  }, [props])
 
   return {
     searchTerm,
     totalPages,
     currentPage,
     handlePageChange,
-    handleSearchChange
-  };
+    handleSearchChange,
+  }
 }
 
 const queriedResults = (
   searchTerm: string,
   SearchResult: (hit: ISearchResult, id: number) => JSX.Element,
-  propsOnlyResults: Pick<IProps, "results">
+  propsOnlyResults: Pick<IProps, 'results'>
 ): JSX.Element | JSX.Element[] => {
-  if (searchTerm === "") return <>{FailedResult("Please search something.")}</>;
+  if (searchTerm === '') return <>{FailedResult('Please search something.')}</>
   if (!propsOnlyResults.results)
-    return <>{FailedResult("Please search something.")}</>; // return if undefined
-  const theResults: ISearchResults = propsOnlyResults.results as ISearchResults;
+    return <>{FailedResult('Please search something.')}</> // return if undefined
+  const theResults: ISearchResults = propsOnlyResults.results as ISearchResults
 
   return (
     <>
       {theResults.hits && theResults.hits.length > 0
         ? theResults.hits.map(SearchResult)
-        : FailedResult("No Results. Please try again.")}
+        : FailedResult('No Results. Please try again.')}
     </>
-  );
-};
+  )
+}
 
 const mapStateToProps = (
   state: IQueryState
-): Pick<IProps, "results" | "activePage" | "queriedTotalPages"> => {
+): Pick<IProps, 'results' | 'activePage' | 'queriedTotalPages'> => {
   return state.query.results
     ? {
         results: state.query.results,
         activePage: state.query.results.page + 1,
-        queriedTotalPages: state.query.results.nbPages - 1 // account for index starting at 0
+        queriedTotalPages: state.query.results.nbPages - 1, // account for index starting at 0
       }
     : {
         results: undefined,
         activePage: 1,
-        queriedTotalPages: 1
-      };
-};
+        queriedTotalPages: 1,
+      }
+}
 
 const mapDispatchToProps = (
   dispatch: Dispatch<dispatchInterface>
-): Pick<IProps, "fetchedResults"> => {
+): Pick<IProps, 'fetchedResults'> => {
   return {
-    fetchedResults: (url: string) => fetchResults(url)(dispatch)
-  };
-};
+    fetchedResults: (url: string) => fetchResults(url)(dispatch),
+  }
+}
 
 // connect the store
 export default connect<
-  Pick<IProps, "results" | "activePage" | "queriedTotalPages">,
-  Pick<IProps, "fetchedResults">,
+  Pick<IProps, 'results' | 'activePage' | 'queriedTotalPages'>,
+  Pick<IProps, 'fetchedResults'>,
   any,
   any
 >(
@@ -167,4 +168,4 @@ export default connect<
   mapDispatchToProps,
   null,
   {}
-)(SearchClient);
+)(SearchClient)
